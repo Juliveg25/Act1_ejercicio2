a78bb97672a7e62721743b3fa3ca76b357465f51
Juliana Vega <83183770+Juliveg25@users.noreply.github.com>
2024-02-18 10:42:28 -0500

    Add files via upload

---
+++ Reloj.java
@@ -0,0 +1,12 @@
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+public class Reloj {
+    Calendar calendar = Calendar.getInstance();
+
+    public void mostrarHora() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String horaActual = sdf.format(calendar.getTime());
+        System.out.println("Current time: " + horaActual);
+    }
+}
